433b0dc2a7ff16e9de4e5f33da93310109118c88
Sandeepmauryaworld <wowinfo.reactdeveloper@gmail.com>
2026-01-02 15:11:46 +0530

    added a icons

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,3 +1,11 @@
+import { MdDashboard } from "react-icons/md";
+import { FaTools } from "react-icons/fa";
+import { FiPieChart } from "react-icons/fi";
+import { IoIosSettings } from "react-icons/io";
+import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
+
+
 export default function LeftSidebar({ onToggle }) {
       return (
             <aside className="left-sidebar">
@@ -10,9 +18,18 @@ export default function LeftSidebar({ onToggle }) {
                   <div className="logo-divider" />
 
                   {/* Menu Buttons */}
-                  <button onClick={onToggle} title="Toggle Sidebar">📚</button>
-                  <button>⚡</button>
-                  <button>🔗</button>
+                  <button
+                        onClick={onToggle}
+                        data-tooltip-id="sidebar-tooltip"
+                        data-tooltip-content="Toggle Sidebar"
+                        className="p-2 text-gray-300 hover:text-white"
+                  >
+                        <MdDashboard size={20} />
+                  </button>
+                  {/* <button onClick={onToggle} title="Toggle Sidebar"><MdDashboard /></button> */}
+                  <button title="Build"><FaTools /></button>
+                  <button title="Monitor"><FiPieChart /></button>
+                  <button title="Manage"><IoIosSettings /></button>
 
                   <div className="sidebar-bottom">
                         <div className="profile">SW</div>
