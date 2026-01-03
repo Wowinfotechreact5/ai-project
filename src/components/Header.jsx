e110d84c8f441b884418efc231d96baba1b1867b
Sandeepmauryaworld <wowinfo.reactdeveloper@gmail.com>
2026-01-03 13:27:07 +0530

    header css replace with tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
 export default function Header() {
       return (
-            <header className="header">
-                  <div className="header-title">
-                        Knowledge Base <span style={{ color: "#9ca3af", fontSize: 12 }}>0 KBs</span>
+            <header style={{
+
+                  padding: '0 20px',
+
+            }} className="
+      h-[60px]
+      flex
+      items-center
+      px-6
+      border-b
+      border-gray-800
+      bg-gradient-to-b
+      from-[#0c111d]
+      to-[#0b1020]
+    ">
+                  <div className="text-base  font-semibold">
+                        Dashboard
                   </div>
             </header>
       );
